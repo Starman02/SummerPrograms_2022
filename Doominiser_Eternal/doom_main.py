04869fc46c2100fd17d03e54cc24c5ea41426ad6
Starman02 <devingrischow2002@gmail.com>
2022-05-22 19:58:20 -0500

    Update doom_main.py

--- Doominiser_Eternal/doom_main.py
+++ Doominiser_Eternal/doom_main.py
@@ -6,7 +6,11 @@ Made by StarMan02
 
 
 import random
+import re
 import sys
+
+
+standby = False
 
 """
 
@@ -38,22 +42,161 @@ class= weapons_randomized
 class Eternal_Weapons_weaponized:
 
 
-    Eternal_Weapons_Easy = {1:'Combat_shotgun', 4:"Rocket_launcher", 5:"Super_Shotgun", 6:"Ballista"}
+    Eternal_Weapons_Easy = {1:'Combat_shotgun', 2: "Heavy_Cannon", 3:"Plasma_Rifle", 4:"Rocket_launcher", 5:"Super_Shotgun", 6:"Ballista",  7:"Chaingun"}
     Eternal_Special_Weapons = {2: "Heavy_Cannon", 3:"Plasma_Rifle", 7:"Chaingun"}
     
 
 
-    def __init__(self, weapon, shooting_count, shooting_time):
-        self.weapon = weapon
+    def __init__(self, weapon = 0, shooting_count = 0, shooting_time = 0):
+        self.weapon = 1
         self.shooting_count = shooting_count
         self.shooting_time = shooting_time
+        
+
+        
+
+    def set_shooting_count(self, shooting_count):
+
+            if self.__weapon == 1:
+                print(self.__weapon)
+                shooting_count = 1
+            
+            elif self.__weapon == 4:
+                print(self.__weapon)
+                shooting_count = 11
+
+            elif self.__weapon == 5:
+                print(self.__weapon)
+                shooting_count = 111
+
+            elif self.__weapon == 6:
+                print(self.__weapon)
+                shooting_count = 1111
+
+            
+            self.shooting_count = shooting_count
+
+
+    def get_shooting_count(self):
+            return self.shooting_count
 
 
 
-    def random_weapon
+
+
+
+    def set_shooting_time(self, shooting_time):
+
+        if self.weapon == 2:
+            print(self.weapon)
+            shooting_time = 3.5
+        
+        elif self.weapon == 3:
+            print(self.weapon)
+            shooting_time = 6
+
+        elif self.weapon == 7:
+            print(self.weapon)
+            shooting_time = 9.5
+        
+        self.__shooting_time = shooting_time
+
+
+
+    def get_shooting_time(self):
+        return self.shooting_time
+
+
+    
+
+
+   
+
+
+    
+    def __str__(self):
+        main_printins = str(self.weapon) + " shotgun blasttttt " + str(self.shooting_count) + "   Shotgungolongus   " +str(self.shooting_time)
+        return main_printins
+        
+    
 
 
 
 
+    
+
+
+
+
+my_squizzle = Eternal_Weapons_weaponized()
+print(my_squizzle)
+
+
+
+
+
+
+
+
+        
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def start_randomized_mode():
+#     standby = True
+
+#     while standby == True:
+#         # programs logic to click and swap
+
+
+
+
+
+
+
+# start_game()
 
     
