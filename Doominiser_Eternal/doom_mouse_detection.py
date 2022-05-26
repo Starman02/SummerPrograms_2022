a36b0bcb62b38b7b9fbea0a0d472821e53b33ebd
Starman02 <devingrischow2002@gmail.com>
2022-05-26 00:04:11 -0500

    Update doom_mouse_detection.py

--- Doominiser_Eternal/doom_mouse_detection.py
+++ Doominiser_Eternal/doom_mouse_detection.py
@@ -7,6 +7,25 @@ when in standbymode, weapon switching will occur.
 """
 
 from doom_main import *
-import pyautigui
+import pyautogui
+import pygame 
+import sys
+
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\n')
+
+
+
+
+    
+
+
 
 
