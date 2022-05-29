b8dc81cd963fadf8927324e4341256d94e75b171
Starman02 <devingrischow2002@gmail.com>
2022-05-29 18:44:47 -0500

    began major updating

--- Doominiser_Eternal/doom_mouse_detection.py
+++ Doominiser_Eternal/doom_mouse_detection.py
@@ -4,21 +4,24 @@ This will only occur when not in standby mode however
 Standby mode is going to be in a while loop, and if the user has not turned on standby mode, then nothing will happen. 
 when in standbymode, weapon switching will occur. 
 
+
+
+
+
 """
 
 
 
-from multiprocessing.connection import wait
-from tkinter import Button
 from doom_main import *
 from pynput.mouse import Listener
+from pynput.keyboard import Key,Controller
 import time
-
+import win32api
 import sys
 
 
 random_weapon_number_generated = Active_weapons_switcher.random_number_generator()
-weapons_number = Active_weapons_switcher(random_weapon_number_generated, random_weapon_number_generated, random_weapon_number_generated)
+weapons_number = Active_weapons_switcher(random_weapon_number_generated, random_weapon_number_generated)
 
 
         
@@ -26,14 +29,7 @@ weapons_number = Active_weapons_switcher(random_weapon_number_generated, random_
 
 clicked = False
 
-# try:
-#     while True:
-#         x, y = pyautogui.position()
-#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-#         print(positionStr, end='')
-#         print('\b' * len(positionStr), end='', flush=True)
-# except KeyboardInterrupt:
-#     print('\n')
+
 print("chink")
 
 standby = True
@@ -41,63 +37,70 @@ standby = True
 
 
 
+
 class Farty:
+
+    keyboard = Controller()
+
+
+    state_left = win32api.GetKeyState(0x01)
+    shot = 0
+
 
 
 
     while standby == True:
-
-        def on_move(x, y):
-            pass
-
-
-        def on_scroll(x, y, dx, dy):
-            pass
+        
+        shoot = win32api.GetKeyState(0x01)
 
 
 
+        if shoot != state_left:
+            state_left = shoot
+            print(shoot)
+            if shoot < 0:
+                shot += 1
+                print(shot)
 
-        def on_click(self, x, y, button, pressed):
+                time.sleep(2)
 
-            self.time_pressed
-            event_time = time()
-
-            if pressed == True:
-
-                self.time_pressed = event_time
-                print(event_time)
-                
-
-
-
-                random_weapon_number_generated = Active_weapons_switcher.random_number_generator()
-                weapons_number = Active_weapons_switcher(random_weapon_number_generated, random_weapon_number_generated, random_weapon_number_generated)
+                keyboard.press("1")
+                keyboard.release("1")
 
 
-                if weapons_number.get_weapon_number() == 2:
+                #if shot == weapons_number.get_weapon_number():
+
+
+                    #win32api.keybd_event(0x35)
+
+
+
+
+                 
+            else:
+                print('left button released')
+
+
+
+
+
+
+
+
+                # if weapons_number.get_weapon_number() == 2:
 
 
 
                     
-                    print( "and so the main weapon number is: " + str(weapons_number.change_shooting_time()))
+                #     print( "and so the main weapon number is: " + str(weapons_number.change_shooting_time()))
 
-                if weapons_number.get_weapon_number() == 3:
-                    print( "and so the main weapon number is: " + str(weapons_number.change_shooting_time()))
+                # if weapons_number.get_weapon_number() == 3:
+                #     print( "and so the main weapon number is: " + str(weapons_number.change_shooting_time()))
 
-                if weapons_number.get_weapon_number() == 7:
-                    print( "and so the main weapon number is: " + str(weapons_number.change_shooting_time()))
-
-            else:
-                pass  
+                # if weapons_number.get_weapon_number() == 7:
+                #     print( "and so the main weapon number is: " + str(weapons_number.change_shooting_time()))
 
 
-
-    
-
-        with Listener(on_move=on_move,on_click=on_click,on_scroll=on_scroll) as listener:
-            listener.join()
-
-        
 
     
 
