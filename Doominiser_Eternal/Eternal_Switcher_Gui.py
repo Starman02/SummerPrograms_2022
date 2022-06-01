53aafba6a7356cd0d05f80c2fc213bb1fbb6b94f
Starman02 <devingrischow2002@gmail.com>
2022-06-01 16:14:46 -0500

    Update Eternal_Switcher_Gui.py

--- Doominiser_Eternal/Eternal_Switcher_Gui.py
+++ Doominiser_Eternal/Eternal_Switcher_Gui.py
@@ -1,14 +1,37 @@
+<<<<<<< HEAD
 
 from tkinter import *
 from PIL import ImageTk, Image
+=======
+import tkinter as tk
+from tkinter import ttk
+from tkinter import * 
+>>>>>>> parent of bba7096 (background image successful)
 
 
 root = Tk()
 
-canv = Canvas(root, width=800, height=900, bg='white')
-canv.grid(row=2, column=3)
+# This is the section of code which creates the main window
+root.geometry('810x530')
+root.configure(background='#9ACD32')
+root.title('Hello, I\'m the main window')
 
-img = ImageTk.PhotoImage(Image.open("D:\Summer_Programs_2021\SummerPrograms_2022\Doominiser_Eternal\senorDoomington.png"))  # PIL solution
-canv.create_image(500, 500, anchor=NW, image=img)
 
+<<<<<<< HEAD
 mainloop()
+=======
+# First, we create a canvas to put the picture on
+jumanji= Canvas(root, height=810, width=530)
+# Then, we actually create the image file to use (it has to be a *.gif)
+picture_file = PhotoImage(file = 'Doominiser_Eternal\senorDoomington.png')  # <-- you will have to copy-paste the filepath here, for example 'C:\Desktop\pic.gif'
+# Finally, we create the image on the canvas and then place it onto the main window
+jumanji.create_image(810, 0, anchor=NE, image=picture_file)
+jumanji.place(x=-1, y=0)
+
+
+# This is the section of code which creates the a label
+Label(root, text='oh yeahhhhhh', bg='#9ACD32', font=('arial', 12, 'normal')).place(x=99, y=70)
+
+
+root.mainloop()
+>>>>>>> parent of bba7096 (background image successful)
