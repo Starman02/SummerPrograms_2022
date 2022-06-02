a5172cfb46ac961270c3d3d96351d8fac309948a
Starman02 <devingrischow2002@gmail.com>
2022-06-02 14:55:16 -0500

    wa

--- Doominiser_Eternal/Eternal_Switcher_Gui.py
+++ Doominiser_Eternal/Eternal_Switcher_Gui.py
@@ -15,15 +15,32 @@ class MainGui:
         self.gui_window = tk.Tk()
 
         self.gui_window.geometry("800x900")
-        self.gui_window.configure(background='gray')
+        self.gui_window.title("The Randomizer Slayer")
 
-        self.img = ImageTk.PhotoImage(file="D:\Summer_Programs_2021\SummerPrograms_2022\Doominiser_Eternal\senorDoomington.png")
+
+        self.img = ImageTk.PhotoImage(file="D:\Summer_Programs_2021\SummerPrograms_2022\Doominiser_Eternal\Damone_moutain.png")
         # background panel
         self.bgpanel = tk.Label(self.gui_window, image=self.img)
+        
+
+
+        # frames for text to go over frames
+        self.title_frame = tk.Frame(self.gui_window)
+        self.button_frame = tk.Frame(self.gui_window) # this frame will also display the status of the gun
+        self.bottom_frame = tk.Frame(self.gui_window)  # this frame will display my name and also potentially a donation box
+
+
+        
+        # label for title
+        self.title_lable = tk.Label(self.title_frame, text="test text")
+
+
+        self.title_lable.pack()
+
+        self.title_frame.pack()
+        self.button_frame.pack()
+        self.bottom_frame.pack()
         self.bgpanel.pack(side='bottom', fill="both", expand="yes")
-
-
-
         self.gui_window.mainloop()
 
 
