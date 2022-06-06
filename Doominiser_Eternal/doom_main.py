ef0f23a2e0169d3646d7b6bae7088964d49b588f
Starman02 <devingrischow2002@gmail.com>
2022-06-06 12:16:28 -0500

    yup

--- Doominiser_Eternal/doom_main.py
+++ Doominiser_Eternal/doom_main.py
@@ -23,22 +23,29 @@ class Active_weapons_switcher:
 
         return random_weapon_number
 
+    def get_weapon_key(self):
+
+        # if self.weapon == 1:
+        #     self.weapon = 1
+        # elif self.weapon == 2:
+        #     self.weapon = 2
+        # elif self.weapon == 3:
+        #     self.weapon = 3
+        # elif self.weapon == 4:
+        #     self.weapon = 4
+        # elif self.weapon == 5:
+        #     self.weapon = 5
+        # elif self.weapon == 6:
+        #     self.weapon = 6
+        # elif self.weapon == 7:
+        #     self.weapon = 7
+
+        return self.weapon
 
 
-    def change_shooting_key(self):
-
-        if self.shooting_count == 1:
-            self.shooting_count = 1
-            
-        elif self.shooting_count == 4:
-            self.shooting_count = 4
-
-        elif self.shooting_count == 5:
-            self.shooting_count = 5
-
-        elif self.shooting_count == 6:
-            self.shooting_count = 6
-
+    def __str__(self):
+        main_printings = str(self.weapon) + " Weapon selection " + str(self.shooting_count) + " weapon Shooting Count " + str(self.shooting_time) + " weapon Shooting Time"
+        return main_printings
 
 
 
@@ -54,3 +61,11 @@ class Active_weapons_switcher:
 # assigns random numbers into the generator
 # should probobly put them in a function
 
+
+print("poverty")
+
+# # these just print the numbers to be generated
+# print( "and so the main weapon number is: " + str(weapons_number.get_weapon_name()))
+# print( "the count number is: " + str(weapons_number.change_shooting_count()))
+# print( "the time number is: " + str(weapons_number.change_shooting_time()))
+    
