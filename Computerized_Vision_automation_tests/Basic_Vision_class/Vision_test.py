c6072809305fbc01eb75845c2f1e5a77f03f174a
Starman02 <devingrischow2002@gmail.com>
2022-06-13 12:43:39 -0500

    yup

---
+++ Computerized_Vision_automation_tests/Basic_Vision_class/Vision_test.py
@@ -0,0 +1,13 @@
+"""
+This program will test the possibilities of computerizeds vision
+RGB Of Healing Cooldown: 97, 7, 23
+"""
+
+
+import pyautogui
+import time
+
+
+time.sleep(1)
+print('begin')
+pyautogui.displayMousePosition()
