be44078ce258e09beee3c55ac6bf0a00d0089097
Starman02 <devingrischow2002@gmail.com>
2022-06-16 18:40:44 -0500

    saved a screnshot

--- Computerized_Vision_automation_tests/Basic_Vision_class/terraria_view.py
+++ Computerized_Vision_automation_tests/Basic_Vision_class/terraria_view.py
@@ -1,5 +1,4 @@
 import pyautogui
 
 
-iml = pyautogui.screenshot(region=(150,150,500,100))
-iml.save(r"D:\Summer_Programs_2021\Computerized_Vision_automation_tests\Basic_Vision_class")
+iml = pyautogui.screenshot('diabetes.png',region=(150,150,500,100))
