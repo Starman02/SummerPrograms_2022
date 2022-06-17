e9e497881419da9e8558a81a5df5e4604e347f5b
Starman02 <devingrischow2002@gmail.com>
2022-06-17 13:24:19 -0500

    updated terraria vision and bots control

--- Computerized_Vision_automation_tests/Basic_Vision_class/Vision_test.py
+++ Computerized_Vision_automation_tests/Basic_Vision_class/Vision_test.py
@@ -3,6 +3,7 @@ This program will test the possibilities of computerizeds vision
 RGB Of Healing Cooldown: 97, 7, 23
 """
 
+from tkinter.messagebox import NO
 from pyautogui import *
 import pyautogui
 import time
@@ -12,7 +13,7 @@ import win32api, win32con
 
 time.sleep(1)
 print('begin')
-pyautogui.displayMousePosition()
+
 
 
 
@@ -27,21 +28,18 @@ pyautogui.displayMousePosition()
 # tests area for the program 
 
 while keyboard.is_pressed('q') == False:
-    flag = 0
-    pic = pyautogui.screenshot(region=(584, 422, 751, 526))
 
-    width, height = pic.size
 
-    # for x in range(0, width, 5):
-    #     for y in range(0, height, 5):
+    if pyautogui.pixelMatchesColor(2123, 43,(221 ,189 ,62)) == True:
+        print("full health")
+    else:
+        print("Damaged")
 
-    #         r, g, b = pic.getpixel((x, y))
 
-    #         if b == 195 and r == 255 and g == 219:
-    #             flag = 1
-    #             click(x+584, y+422)
-    #             time.sleep(0.05)
-    #             break
+    if pyautogui.locateOnScreen('healing_cooldown.png', confidence=.8) != None:
+        print("ON SCREEN")
 
-    #     if flag == 1:
-    #         break
+    else:
+        print("NOT ON SCREEN")
+
+    
--- Computerized_Vision_automation_tests/Basic_Vision_class/terraria_view.py
+++ Computerized_Vision_automation_tests/Basic_Vision_class/terraria_view.py
@@ -1,4 +1,9 @@
 import pyautogui
 
 
-iml = pyautogui.screenshot('diabetes.png',region=(150,150,500,100))
+# iml = pyautogui.screenshot('diabetes.png',region=(50,40,2500,300))
+pyautogui.displayMousePosition()
+
+
+# x: 2123 y:43
+# yellow: 221 189 62
