bfbaa6a08337f04ad799a929ffc342f5e0a5d8cd
Starman02 <devingrischow2002@gmail.com>
2022-06-27 13:16:34 -0500

    test demo made

--- Game_Designing/Game_tests.py
+++ Game_Designing/Game_tests.py
@@ -4,16 +4,19 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 game_test = Ursina()
 
 
-for z in range(20):
-    for x in range(20):
-        Entity(
-            model="cube", color=color.dark_gray, collider="box", ignore=True,
-            position = (x, 0, z),
-            parent=scene,
-            orgin_y = 0.5,
-            texture = "white_cube")
+ground = Entity(model="plane", texture="grass", collider="mesh", scale=(200, 1, 200))
 
-robo_entity = Entity(model="D:\Summer_Programs_2021\Assets\deathbot.obj", scale=2, y=2, z=1)
+Sky()
+# for z in range(20):
+#     for x in range(20):
+#         Entity(
+#             model="cube", color=color.dark_gray, collider="box", ignore=True,
+#             position = (x, 0, z),
+#             parent=scene,
+#             orgin_y = 0.5,
+#             texture = "white_cube")
+
+robo_entity = Entity(model=r'Assets\finnaly_letri.obj', texture=r'Assets\DefaultMaterial_Base_Color.png', scale=5, y=3, x = 2, z=.11)
 
 
 player = FirstPersonController()
