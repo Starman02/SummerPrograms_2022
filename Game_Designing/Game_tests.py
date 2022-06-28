3bf55c191620da86ce37a7c804db54ffd05f679b
Starman02 <devingrischow2002@gmail.com>
2022-06-28 17:48:43 -0500

    got gun to load in, workubg on shooting

--- Game_Designing/Game_tests.py
+++ Game_Designing/Game_tests.py
@@ -1,5 +1,7 @@
+from turtle import position
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+import time
 
 game_test = Ursina()
 
@@ -20,7 +22,41 @@ robo_entity = Entity(model=r'Assets\finnaly_letri.obj', texture=r'Assets\Default
 
 
 
-weapon = Entity(model=r'Assets\improvedwireframe.obj', parent=camera.ui, scale=2.5,color=color.gold, texure="white_cube", position=(0.8, -0.6))
+# weapon = Entity(model=r'Assets\improvedwireframe.obj', parent=camera.ui, scale=5,color=color.gold, texure="white_cube", position=(0.8, -0.6), rotation=(-10,-20,-10))
 
-camera.z = -5
+
+class Player(Entity):
+    def __init__(self, **kwargs):
+        self.controller = FirstPersonController(**kwargs)
+        super().__init__(parent=self.controller)
+
+        self.scifi_shotgun = Entity(parent=self.controller.camera_pivot, scale=4, position=Vec3(0.7,-1,1.5), model=r'Assets\scifi_shotty.obj', texture=r"Assets\shotgun_color.png.png")
+        self.controller.speed = 20
+
+    def input(self, key):
+        if key == "left mouse down":
+            Bullet(model="sphere", color=color.black, scale=0.3, position=self.controller.camera_pivot.world_position, rotation=self.controller.camera_pivot.world_ro)
+
+
+
+
+
+class Bullet(Entity):
+    def __init__(self, speed=50, lifetime=10, **kwargs):
+        super().__init__(**kwargs)
+        self.lifetime = lifetime
+        self.speed = speed
+        self.start = time.time()
+
+    def update(self):
+        ray = (self.world_position, self.forward, distance= self.speed * time.dt)
+        if not ray.hit and time.time() - self.start < self.lifetime:
+            self.world_position += self.forward * self.speed * time.dt
+        else:
+            destroy(self)
+
+
+        
+        
+player1 = Player(position=(0,10,0))
 game_test.run()
