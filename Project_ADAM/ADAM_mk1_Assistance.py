28cc24020f936b01a3467d661b3412f72bdd6a7d
Starman02 <devingrischow2002@gmail.com>
2022-07-05 15:29:32 -0500

    started ADAM GUI module

--- Project_ADAM/ADAM_mk1_Assistance.py
+++ Project_ADAM/ADAM_mk1_Assistance.py
@@ -53,6 +53,7 @@ class Assist_Module_MK1:
         
         # for every item in the test list, do an action
         if "class" in self.user_entry_assist:
+            ####################################################################################################################################################################################
             self.class_assist_window_1 = tkinter.Toplevel() # creates another window for ADAM
             self.class_frame1 = tkinter.Frame(self.class_assist_window_1)
             self.button_frame_left = tkinter.Frame(self.class_assist_window_1)
@@ -67,7 +68,7 @@ class Assist_Module_MK1:
             # copy and paste storage
             #an empty dictionary
  
-            
+            ###potential idea for bigger libraries, store them in .dat files, could also be used to store them in a dictionary
             dictionary_storage = {}
             f = open('Project_ADAM\STORAGE\copy_and_paste_samples.txt', 'r')
             for line in f.readlines():
@@ -83,11 +84,29 @@ class Assist_Module_MK1:
     self.X = X"""
 
             # entire class demo with objects
-            self.c4 = """class Demo:"""
+            self.c4 = """class Demo:
+    def __init(self, X):
+        self.X = X
+        
+        
+    def set_X(self, X):
+        self.X = X
+
+
+    def get_X(self):
+        return self.X
+        """
+            self.c5 = """def __str__(self):
+    return "the value of X is " + self.X
+            """
+
+        
             ####
             self.self_help_button = tkinter.Button(self.button_frame_left,text="Copy: self", command= lambda: self.action_button(self.c1))
             self.getter_example_button = tkinter.Button(self.button_frame_left, text="Copy: get_attribute example", command= lambda: self.action_button(self.c2))
             self.setter_example_button = tkinter.Button(self.button_frame_left, text="Copy: set_attribute example", command= lambda:self.action_button(self.c3))
+            self.demo_button = tkinter.Button(self.button_frame_left, text="Copy: Class Demo", command= lambda:self.action_button(self.c4))
+            self.string_returning_demo = tkinter.Button(self.button_frame_left, text="Copy: String Printing function", command= lambda:self.action_button(self.c5))
 
 
 
@@ -95,14 +114,94 @@ class Assist_Module_MK1:
             self.self_help_button.pack()
             self.getter_example_button.pack()
             self.setter_example_button.pack()
+            self.string_returning_demo.pack()
+            self.demo_button.pack()
             self.button_frame_left.pack(side='left')
             self.class_frame1.pack()
+            ################################################################
+
+
+
+
+
+
+
+
+
+        if "ADAM GUI" in self.user_entry_assist:
+            #################################################################################################
+            self.ADAMGUI_ASSIST = tkinter.Toplevel() # creates another window for ADAM
+            self.ADAMGUI_frame1 = tkinter.Frame(self.ADAMGUI_ASSIST)
+            self.button_frame_left2 = tkinter.Frame(self.ADAMGUI_frame1)
+
+            self.ADAMGUI_ASSIST.geometry('500x300+900+300')
+
+            self.assistance_label2= tkinter.StringVar()
+
+            self.assistance_label2.set("ADAM GUI Aiding Modules")
+            self.label_assist_setting2 = tkinter.Label(self.ADAMGUI_frame1, textvariable = self.assistance_label2)
+
+
+
+
+
+            #### Storage for ADAMGUI MODULES###
+
+            
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+            self.self_help_button = tkinter.Button(self.button_frame_left,text="Copy: self", command= lambda: self.action_button(self.c1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     def action_button(self, text_placeholder):
         
         pyperclip.copy(text_placeholder)
-        self.assistance_label1.set(text_placeholder + " Action has been Preformed")
+        self.assistance_label1.set("Action has been Preformed")
 
 
         
